e435a91d32f6eeb8235149dce449834c52809c31
slackbot115 <viniccius.carvalho22@gmail.com>
2019-04-18 03:13:13 +0900

    Mask e select do ano funcionando

--- www/components/banco.js
+++ www/components/banco.js
@@ -2,6 +2,11 @@ $(function() {
     $('#valor').maskMoney();
   })
 
+var ano = "";
+$(document).on('change', '#ano', function(){
+  ano = $("select option:selected").val();
+});
+
 $(document).on('click','#cadastro',function(){
   if($('#marca').val() == "" || $('#modelo').val() == "" || $('#ano').val() == "" || $('#cor').val() == "" || $('#valor').val() == ""){
     alert("Existem campos ainda nao preenchidos, preencha-os!");
@@ -10,27 +15,26 @@ $(document).on('click','#cadastro',function(){
     var parametros = {
     "marca": $("#marca").val(),
     "modelo": $("#modelo").val(),
-    "ano": $("#ano").val(),
+    "ano": ano,
     "cor": $("#cor").val(),
     "valor": $("#valor").val()
   }
-  //   $.ajax({
-  //   type:"post",
-  //   url:"https://cadastro-de-carros-slackbot115.c9users.io/conexao.php",
-  //   data:parametros,
-  //   success:function(data){
-  //     $("#marca").val("");
-  //     $("#modelo").val("");
-  //     $("#ano").val("");
-  //     $("#cor").val("");
-  //     $("#valor").val("");
-  //     navigator.notification.alert(data);
-  //   },
-  //   error:function(data){
-  //     navigator.notification.alert(data);
-  //   }
-  // });
-  console.log(parametros);
+     $.ajax({
+     type:"post",
+     url:"https://cadastro-de-carros-slackbot115.c9users.io/conexao.php",
+     data:parametros,
+     success:function(data){
+       $("#marca").val("");
+       $("#modelo").val("");
+       $("#ano").val("");
+       $("#cor").val("");
+       $("#valor").val("");
+       navigator.notification.alert(data);
+     },
+     error:function(data){
+       navigator.notification.alert(data);
+     }
+   });
   }
 });
 
